f3f7164ad758d9d856afd82dad2746fd2288f63c
nithish125 <nithishs3620@gmail.com>
2022-08-20 11:07:11 +0530

    modified.

---
+++ add.py
@@ -0,0 +1,72 @@
+print("hi")
+
+# arithmatic operator
+
+x=15
+y=6
+z=x+y
+print(z)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+
+# comparision operator
+
+print(x==y)
+print(x!=y)
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+
+# logical operator
+
+print(x > 3 and x < 10)
+print(x > 3 or x < 10)
+print (not(x > 3 and x < 10))
+
+#assignment operator
+x=5
+print(x)
+x+=3
+print(x)
+x-=4
+print(x)
+x*=6
+print(x)
+x/=4
+print(x)
+x%=4
+print(x)
+x//=5
+print(x)
+x**=7
+print(x)
+
+# Identity Operators
+
+x = ["apple", "banana"]
+y = ["apple", "mango"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x is not z)
+print(x is not y)
+
+#membership operator
+
+a=["banana","orange"]
+print("apple" in a)
+print("apple" not in a)
+print("orange" not in a)
+print("orange"  in a)
+
+
+
+
+
+ 
